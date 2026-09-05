c01af80cac833156dfcb6503e9b0df2281c5d4e8
Yeison Moran <moranyeison522@gmail.com>
2026-09-04 20:08:30 -0500

    login checkbos new 5

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Form from 'react-bootstrap/Form';
+
 import axios from "axios";
 
 function Login() {
@@ -50,7 +50,7 @@ function Login() {
                             <div className="card-body p-0">
                                 <div className="row">
 
-
+                             
                                     <div className="col-lg-6 d-none d-lg-flex align-items-center justify-content-center bg-primary">
                                         <img
                                             src="./img/Logo.png"
@@ -59,7 +59,7 @@ function Login() {
                                         />
                                     </div>
 
-
+                                   
                                     <div className="col-lg-6">
                                         <div className="p-5">
                                             <div className="text-center">
@@ -87,12 +87,9 @@ function Login() {
                                                 <button type="submit" className="btn btn-primary btn-user btn-block w-100">
                                                     Iniciar sesión
                                                 </button>
-                                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                                    <Form.Check type="checkbox" label="Visualizar contraseña" />
-                                                </Form.Group>
-                                                {error && (
-                                                    <div className="alert alert-danger mt-3 text-center">{error}</div>
-                                                )}
+                                            {error && (
+                                                <div className="alert alert-danger mt-3 text-center">{error}</div>
+                                            )}
                                         </div>
                                     </div>
 
