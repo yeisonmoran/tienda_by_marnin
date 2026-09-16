636e8e77d98e1055a01c6ac6d76552d709cdbb3b
Yeison Moran <moranyeison522@gmail.com>
2026-09-16 18:30:14 -0500

    Codificacion de estado en lista cliente

--- Frontend/src/listClientes.jsx
+++ Frontend/src/listClientes.jsx
@@ -45,7 +45,7 @@ function Clientes() {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then(() => {
-        setClientes(clientes.map((cli) => cli.id_cliente === id ? { ...cli, activo: false } : cli));
+        setClientes(clientes.map(cli => cli.id_cliente === id ? { ...cli, activo: false } : cli));
       })
       .catch((error) => {
         if (error.response && error.response.status === 403) {
