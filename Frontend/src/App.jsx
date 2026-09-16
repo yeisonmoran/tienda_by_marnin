eacb2b241ac0a118236048095ac69189cb328c78
Yeison Moran <moranyeison522@gmail.com>
2026-09-16 12:18:56 -0500

    Renombramiento de ruta detalle

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
             <Route path="/list-ventas" element={<Ventas />} />
             <Route path="/inventario" element={<Inventario />} />
             <Route path="/no-autorizado" element={<NoAutorizado />}></Route>
-            <Route path="/venta/detalle/:id" element={<Detalle />}></Route>
+            <Route path="/ventas/detalle/:id" element={<Detalle />}></Route>
 
             <Route element={<RutaAdmin />}>
               <Route path="/list-usuarios" element={<Usuarios />} />
